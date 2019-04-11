145fa92d4f7329296083546817a28c31c977ba0c
Yanis48 <doublecraft.official@gmail.com>
2019-04-11 16:19:33 +0200

    Updated to 1.14-pre1, loader 0.4.0
    Added ignoreEntities argument too

--- src/main/java/com/yanis48/handystructures/command/StructureCommand.java
+++ src/main/java/com/yanis48/handystructures/command/StructureCommand.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
@@ -50,17 +51,21 @@ public class StructureCommand {
 		commandDispatcher_1.register((ServerCommandManager.literal("structure").requires((serverCommandSource_1) -> {
 			return serverCommandSource_1.hasPermissionLevel(2);
 		})).then(ServerCommandManager.literal("load").then(ServerCommandManager.argument("name", IdentifierArgumentType.create()).then(ServerCommandManager.argument("pos", BlockPosArgumentType.create()).executes((context) -> {
-			return loadStructure(context.getSource(), IdentifierArgumentType.getIdentifierArgument(context, "name"), BlockPosArgumentType.getPosArgument(context, "pos"), 0, "none");
+			return loadStructure(context.getSource(), IdentifierArgumentType.getIdentifierArgument(context, "name"), BlockPosArgumentType.getPosArgument(context, "pos"), 0, "none", true);
 		}).then(ServerCommandManager.argument("rotation", IntegerArgumentType.integer()).suggests(ROTATION_SUGGESTIONS).executes((context) -> {
-			return loadStructure(context.getSource(), IdentifierArgumentType.getIdentifierArgument(context, "name"), BlockPosArgumentType.getPosArgument(context, "pos"), IntegerArgumentType.getInteger(context, "rotation"), "none");
+			return loadStructure(context.getSource(), IdentifierArgumentType.getIdentifierArgument(context, "name"), BlockPosArgumentType.getPosArgument(context, "pos"), IntegerArgumentType.getInteger(context, "rotation"), "none", true);
 		}).then(ServerCommandManager.argument("mirror", StringArgumentType.string()).suggests(MIRROR_SUGGESTIONS).executes((context) -> {
-			return loadStructure(context.getSource(), IdentifierArgumentType.getIdentifierArgument(context, "name"), BlockPosArgumentType.getPosArgument(context, "pos"), IntegerArgumentType.getInteger(context, "rotation"), StringArgumentType.getString(context, "mirror"));
-		})))))).then(ServerCommandManager.literal("save").then(ServerCommandManager.argument("from", BlockPosArgumentType.create()).then(ServerCommandManager.argument("to", BlockPosArgumentType.create()).then(ServerCommandManager.argument("name", IdentifierArgumentType.create()).executes((context) -> {
-			return saveStructure(context.getSource(), BlockPosArgumentType.getPosArgument(context, "from"), BlockPosArgumentType.getPosArgument(context, "to"), IdentifierArgumentType.getIdentifierArgument(context, "name"));
-		}))))));
+			return loadStructure(context.getSource(), IdentifierArgumentType.getIdentifierArgument(context, "name"), BlockPosArgumentType.getPosArgument(context, "pos"), IntegerArgumentType.getInteger(context, "rotation"), StringArgumentType.getString(context, "mirror"), true);
+		}).then(ServerCommandManager.argument("ignoreEntities", BoolArgumentType.bool()).executes((context) -> {
+			return loadStructure(context.getSource(), IdentifierArgumentType.getIdentifierArgument(context, "name"), BlockPosArgumentType.getPosArgument(context, "pos"), IntegerArgumentType.getInteger(context, "rotation"), StringArgumentType.getString(context, "mirror"), BoolArgumentType.getBool(context, "ignoreEntities"));
+		}))))))).then(ServerCommandManager.literal("save").then(ServerCommandManager.argument("from", BlockPosArgumentType.create()).then(ServerCommandManager.argument("to", BlockPosArgumentType.create()).then(ServerCommandManager.argument("name", IdentifierArgumentType.create()).executes((context) -> {
+			return saveStructure(context.getSource(), BlockPosArgumentType.getPosArgument(context, "from"), BlockPosArgumentType.getPosArgument(context, "to"), IdentifierArgumentType.getIdentifierArgument(context, "name"), true);
+		}).then(ServerCommandManager.argument("ignoreEntities", BoolArgumentType.bool()).executes((context) -> {
+			return saveStructure(context.getSource(), BlockPosArgumentType.getPosArgument(context, "from"), BlockPosArgumentType.getPosArgument(context, "to"), IdentifierArgumentType.getIdentifierArgument(context, "name"), BoolArgumentType.getBool(context, "ignoreEntities"));
+		})))))));
 	}
 
-	public static int loadStructure(ServerCommandSource source, Identifier name, BlockPos pos, int rot, String mir) throws CommandSyntaxException {
+	public static int loadStructure(ServerCommandSource source, Identifier name, BlockPos pos, int rot, String mir, boolean ignoreEntities) throws CommandSyntaxException {
 		int int_1 = 0;
 		World world = source.getWorld();
 		BlockPos offset = new BlockPos(0, 1, 0);
@@ -100,7 +105,7 @@ public class StructureCommand {
 			ServerWorld serverWorld = (ServerWorld)world;
 			StructureManager structureManager = serverWorld.getStructureManager();
 			Structure structure = structureManager.getStructure(name);
-			StructurePlacementData placementData = (new StructurePlacementData()).setMirrored(mirror).setRotation(rotation).setIgnoreEntities(true).setChunkPosition((ChunkPos)null);
+			StructurePlacementData placementData = (new StructurePlacementData()).setMirrored(mirror).setRotation(rotation).setIgnoreEntities(ignoreEntities).setChunkPosition((ChunkPos)null);
 			
 			if (structureManager.getStructure(name) != null) {
 				structure.place(world, pos_2, placementData);
@@ -112,7 +117,7 @@ public class StructureCommand {
 		return int_1;
 	}
 
-	public static int saveStructure(ServerCommandSource source, BlockPos from, BlockPos to, Identifier name) throws CommandSyntaxException {
+	public static int saveStructure(ServerCommandSource source, BlockPos from, BlockPos to, Identifier name, boolean ignoreEntities) throws CommandSyntaxException {
 		int int_1 = 0;		
 		World world = source.getWorld();
 		int x1 = from.getX() < to.getX() ? from.getX() : to.getX();
@@ -123,7 +128,6 @@ public class StructureCommand {
 		int y2 = from.getY() < to.getY() ? to.getY() - from.getY() : from.getY() - to.getY();
 		int z2 = from.getZ() < to.getZ() ? to.getZ() - from.getZ() : from.getZ() - to.getZ();
 		BlockPos size = new BlockPos(x2, y2, z2);
-		boolean ignoreEntities = true;
 		
 		if (!world.isClient && name != null) {
 			ServerWorld serverWorld = (ServerWorld)world;
